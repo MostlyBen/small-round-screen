24e74538e182ab5fee530533f22582bc5985ad8c
Ben Hoff <benthehoff@gmail.com>
2024-07-27 18:24:00 -0400

    added support for command line arguments '-gap' and '-padding_fill'

--- pixel-image.py
+++ pixel-image.py
@@ -1,21 +1,35 @@
 from PIL import Image
+import sys
 import os
 
 # Function to write text to framebuffer
-def write_to_framebuffer(framebuffer='/dev/fb1', width=240, height=240, pixel_size=5, gap=2, padding=9, resolution=32):
+def write_to_framebuffer(framebuffer='/dev/fb1', width=240, height=240, gap=2, resolution=32):
+    # get arguments 
+    if "-gap" in sys.argv:
+        gap = sys.argv[sys.argv.index("-gap") + 1]
+        gap = int(gap)
+    if "-padding_fill" in sys.argv:
+        if sys.argv[sys.argv.index("-padding_fill") + 1] == "white":
+            padding_fill = 255
+        else:
+            padding_fill = 0
+ 
+    # determine variables
+    pixel_size = (width // resolution) - gap
+
+    padding = width - ((width // resolution) * resolution)
+    padding = -(padding // -2) + 1
+
     image = Image.open('./sad-pepe.png')
     image = image.convert('RGB')
 
+    # initialize bytarray
     data = bytearray()
 
     for x in range(width):
         for y in range(height):
-            # top/left/right/bottom padding
-            if y <= padding or x <= padding or x > (width - padding) or y > (height - padding):
-                data.append(0)
-                data.append(0)
-                continue
-
+            
+            # gap
             x_in_px_gap_sequence = (x - padding) % (pixel_size + gap)
             y_in_px_gap_sequence = (y - padding) % (pixel_size + gap)
             if ((pixel_size + gap) - x_in_px_gap_sequence) < gap or ((pixel_size + gap) - y_in_px_gap_sequence) < gap:
@@ -23,21 +37,28 @@ def write_to_framebuffer(framebuffer='/dev/fb1', width=240, height=240, pixel_si
                 data.append(0)
                 continue
 
+            # top/left/right/bottom padding
+            if y <= padding or x <= padding or x > (width - padding) or y > (height - padding):
+                data.append(padding_fill)
+                data.append(padding_fill)
+                continue
+
+            # get location
             row = (y - padding) // (pixel_size + gap)
             col = (x - padding) // (pixel_size + gap)
-            # value = 255 * pixel_array[col][row]
-            # data.append(value)
-            # data.append(value)
 
+            # get pixel & convert to 16 bit
             r, g, b = image.getpixel((row, col))
-            # convert RGB888 to RGB565
             rgb565 = ((r & 0xf8) << 8) | ((g & 0xfc) << 3) | (b >> 3)
-            # screen expects bytes in little-endian order (low byte first)
+
+            # write to bytearray 
             data.append(rgb565 & 0xff)        # low byte
             data.append((rgb565 >> 8) & 0xff) # high byte
 
+    # Write to framebuffer
     with open(framebuffer, 'wb') as f:
         f.write(data)
 
-# write input to the framebuffer
+# write image to the framebuffer
 write_to_framebuffer()
+
